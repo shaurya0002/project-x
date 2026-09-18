9f8d396d9c97d6bb00b49bd538692db86d15eeae
shaurya0002 <shauryapandey010182@gmail.com>
2026-09-18 20:39:12 +0530

    changed event section and about section position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ export default function Home(){
     return(
         <>
         <Hero />
-        <About />
         <Events />
+        <About />
         <Lineup />
         <Gallery />
         </>
